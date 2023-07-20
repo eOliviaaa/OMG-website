6c8770d3e22754c13d2ab35b371ecc2b830cc0a0
eoliviaaa <eoliviaaaa@gmail.com>
2023-07-20 10:15:07 +0200

    minor changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import Footer from "../components/footer/Footer.js";
-// import EventCarousel from "../components/home-main/EventCarousel.js";
 import Welcome from "../components/home-main/Welcome.js";
 import Events from "../components/home-main/Events.js"
 
@@ -7,8 +6,12 @@ export default function Home() {
     return (
         <body>
             <main>
-                <Welcome />
-                <Events />
+                <div>
+                    <Welcome />
+                </div>
+                <div>
+                    <Events />
+                </div>
             </main>
             <Footer />
         </body>
